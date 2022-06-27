559f671134ccd2572fcec4e15604280a09f68d0e
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-27 22:35:43 +0800

    feat: init runtime approach

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export const one = 1
-export const two = 2
+import { resolve as resolvePackagePath } from 'mlly'
+
+export interface GetExportsOptions {
+  url?: string
+}
+
+export async function getExports(name: string, options?: GetExportsOptions) {
+  const path = await resolvePackagePath(name, { url: options?.url })
+  const pkg = await import(path)
+  const keys = Object.keys(pkg)
+  if (keys.length === 1 && keys[0] === 'default')
+    return Object.keys(pkg.default)
+  return keys
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,62 @@
+import { resolve as resolvePackagePath } from 'mlly'
 import { describe, expect, it } from 'vitest'
+import { getExports } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('ESM', () => {
+  it('@antfu/utils', async () => {
+    expect((await getExports('@antfu/utils')).slice(0, 5))
+      .toMatchInlineSnapshot(`
+        [
+          "assert",
+          "at",
+          "batchInvoke",
+          "clamp",
+          "clampArrayRange",
+        ]
+      `)
+  })
+
+  it('@vue/shared', async () => {
+    expect((await getExports('@vue/shared', {
+      url: await resolvePackagePath('vue'),
+    })).slice(6, 12))
+      .toMatchInlineSnapshot(`
+        [
+          "camelize",
+          "capitalize",
+          "def",
+          "default",
+          "escapeHtml",
+          "escapeHtmlComment",
+        ]
+      `)
+  })
+})
+
+describe('CJS', () => {
+  it('axios', async () => {
+    expect((await getExports('axios')).slice(0, 5))
+      .toMatchInlineSnapshot(`
+        [
+          "request",
+          "getUri",
+          "delete",
+          "get",
+          "head",
+        ]
+      `)
+  })
+
+  it('react', async () => {
+    expect((await getExports('react')).slice(0, 5))
+      .toMatchInlineSnapshot(`
+        [
+          "Children",
+          "Component",
+          "Fragment",
+          "Profiler",
+          "PureComponent",
+        ]
+      `)
   })
 })
