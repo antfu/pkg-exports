a4600cf6a8a3b2fb48430760ad27d6d6623eb9df
三咲智子 <sxzz@sxzz.moe>
2022-08-02 02:55:29 +0800

    fix: cjs compact (#2)

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,11 +1,12 @@
-import Pool from 'tinypool'
+import type Pool from 'tinypool'
 import type { GetExportsOptions } from './types'
 
 let _worker: Pool | undefined
 
 export async function getExportsRuntime(name: string, options?: GetExportsOptions) {
   if (!_worker) {
-    _worker = new Pool({
+    const { Tinypool } = await import('tinypool')
+    _worker = new Tinypool({
       filename: new URL('./worker.js', import.meta.url).href,
     })
   }
