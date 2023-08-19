f81ba3479bbd2d269f00a2cadaaaae2b28347661
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-19 17:47:52 +0800

    fix: package regex

--- src/static.ts
+++ src/static.ts
@@ -9,7 +9,7 @@ export async function getExportsStatic(name: string, options?: GetExportsOptions
   const map = new Map<string, Promise<string[]>>()
 
   async function resolveEntry(name: string, importer?: string) {
-    if (name.match(/^[@a-z0-9]/)) {
+    if (name.match(/^[a-z0-9-._~]/)) {
       try {
         const { rootPath, packageJson } = (await getPackageInfo(name, { paths: importer ? [fileURLToPath(importer)] : undefined }))!
         if (!packageJson.exports && packageJson.module)
